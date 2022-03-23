37002f63209f12889871a91a55990f1e4398062f
Evgeniy Nekrasov <eanekrasovart@gmail.com>
2022-03-23 22:36:43 +0100

    adding dad jokes api

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
-// const minutesFirst = prompt("Introduce los minutos: ");
-
-const minutesFirst = Swal.fire({
-    title: "Hola",
-    input: "number",
-})
-let time = minutesFirst * 60
+const minutesFirst = prompt("Introduce los minutos: ");
+let time = minutesFirst * 60;
 
 const countDown = document.getElementById("countDown");
 
 const updateCountDown = () => {
-    const min = Math.floor(time / 60);
-    let sec = time % 60;
+  const min = Math.floor(time / 60);
+  let sec = time % 60;
 
-    sec = sec < 10 ? '0' + sec : sec;
-    countDown.innerHTML = `${min} : ${sec}`;
-    time--;
-    if(time < 0) {
-        Swal.fire({
-            title: "Se acabo"
-        });
-        clearInterval(timer);
-    }
-}
+  sec = sec < 10 ? "0" + sec : sec;
+  countDown.innerHTML = `${min} : ${sec}`;
+  time--;
+  if (time < 0) {
+    Swal.fire({
+      title: "BOOOOM",
+      text: "Time is out!",
+      imageUrl:
+        "https://images.unsplash.com/photo-1507048947301-7afc2aca0edc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: "Custom image",
+    });
+    clearInterval(timer);
+  }
+};
 
 const timer = setInterval(updateCountDown, 1000);
 
-// CHANGE BACKGROUND 
-let clientId = "wztBaWIMpJxQd0Eql30sjRYmQ9x1915oaFOwI683Cts";
-let requestUrl = `https://api.unsplash.com/photos/random/?client_id=${clientId}`;
-let imageElement = document.querySelector("#unsplashImage")
-let imageLink = document.querySelector("#imageLink");
-let creator = document.querySelector("#creator");
+const jokeText = document.querySelector(".dadJoke");
 
-fetch(requestUrl)
-    .then(function(res) {
-        return res.json();
-    })
-    .then(function (jsonData) {
-        imageElement.src = jsonData.urls.regular;
-        imageLink.setAttribute("href", jsonData.links.html);
+async function generateJoke() {
+  const jokesRes = await fetch("https://icanhazdadjoke.com", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
 
-        creator.innerText = jsonData.user.name;
-        creator.setAttribute("href", jsonData.user.portfolio_url)
-    }) 
+  const jokePut = await jokesRes.json();
+  jokeText.innerHTML = jokePut.joke;
+}
+
+generateJoke();
+setInterval(generateJoke, 5000);
